001e6d64ec6a1c1940382353f85827838924c025
Marcos Vinicius <viniciusmr1499@gmail.com>
2020-09-14 23:22:23 -0300

    desafio finalizado

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,41 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  ManyToOne
+} from 'typeorm';
+
+import Category from './Category';
+
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('float')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
